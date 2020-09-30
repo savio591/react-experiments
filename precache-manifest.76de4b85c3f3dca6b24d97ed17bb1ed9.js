ce5b532476606e5d40ac1a6be39f706d0d097499
savio591 <savio591@hotmail.com>
2020-09-30 19:42:06 -0300

    Updates

---
+++ precache-manifest.76de4b85c3f3dca6b24d97ed17bb1ed9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8a521d7bd35f4f75290a9c0b46518c",
+    "url": "/react-experiments/index.html"
+  },
+  {
+    "revision": "cdb281e9a6cce5717461",
+    "url": "/react-experiments/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "12496789899ecc8f8cdc",
+    "url": "/react-experiments/static/js/2.fb4ad0f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-experiments/static/js/2.fb4ad0f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb281e9a6cce5717461",
+    "url": "/react-experiments/static/js/main.2817a92a.chunk.js"
+  },
+  {
+    "revision": "6c2795d8c1c8752e362c",
+    "url": "/react-experiments/static/js/runtime-main.6eb6a10f.js"
+  }
+]);
